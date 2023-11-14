aeabe13f38a06f66df70effab981562532ba9564
Naomi Crutcher <seriouschild99@gmail.com>
2023-11-14 13:49:37 -0600

    Delete group_project0 directory

--- group_project0/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.group_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.group_project to javafx.fxml;
-    exports com.example.group_project;
-}
